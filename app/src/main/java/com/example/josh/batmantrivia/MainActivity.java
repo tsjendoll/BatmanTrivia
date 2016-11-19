ab4eb3ce0e362788e64139495b494213395175ce
Josh Berenguel <joshberenguel@gmail.com>
2016-11-18 16:45:47 -0800

    Add EditText field for resubmission

--- app/src/main/java/com/example/josh/batmantrivia/MainActivity.java
+++ app/src/main/java/com/example/josh/batmantrivia/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
@@ -18,15 +19,16 @@ public class MainActivity extends AppCompatActivity {
     public void calculateScore(View view) {
         int score =0;
 
+        EditText editText = (EditText) findViewById(R.id.name_edit);
+
         RadioButton rb1 = (RadioButton) findViewById(R.id.radio_Bruce_Wayne);
         RadioButton rb2 = (RadioButton) findViewById(R.id.radio_Bob_Kane);
         RadioButton rb3 = (RadioButton) findViewById(R.id.radio_Bane);
         RadioButton rb4 = (RadioButton) findViewById(R.id.radio_bat_cow);
         RadioButton rb5 = (RadioButton) findViewById(R.id.radio_Joe_Chill);
         RadioButton rb6 = (RadioButton) findViewById(R.id.radio_killing_joke);
-        RadioButton rb7 = (RadioButton) findViewById(R.id.radio_oracle);
-        RadioButton rb8 = (RadioButton) findViewById(R.id.radio_ras);
-        RadioButton rb9 = (RadioButton) findViewById(R.id.radio_Talia);
+        RadioButton rb7 = (RadioButton) findViewById(R.id.radio_ras);
+        RadioButton rb8 = (RadioButton) findViewById(R.id.radio_Talia);
 
         CheckBox checkBox_1 = (CheckBox) findViewById(R.id.checkbox_Brown);
         CheckBox checkBox_2 = (CheckBox) findViewById(R.id.checkbox_Damian);
@@ -34,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         CheckBox checkBox_4 = (CheckBox) findViewById(R.id.checkbox_Kelley);
         CheckBox checkBox_5 = (CheckBox) findViewById(R.id.checkbox_Todd);
 
-        RadioButton[] radio_array = {rb1,rb2,rb3,rb4,rb5,rb6,rb7,rb8,rb9};
+        RadioButton[] radio_array = {rb1,rb2,rb3,rb4,rb5,rb6,rb7,rb8};
 
         CheckBox[] checkbox_array = {checkBox_1,checkBox_2,checkBox_3,checkBox_4,checkBox_5};
 
@@ -46,6 +48,10 @@ public class MainActivity extends AppCompatActivity {
         for (CheckBox checkbox:checkbox_array) {
             if (checkbox.isChecked())
                 score ++;
+        }
+
+        if (editText.getText().toString().toUpperCase().equals("ORACLE")) {
+            score++;
         }
 
         Toast.makeText(this,"You scored " + score + " out of 14",Toast.LENGTH_LONG).show();
